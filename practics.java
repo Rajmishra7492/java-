26ff05c9d79ed2d30795beb781252b296f62aa5f
day planner <rajmishra273165mkh@gmail.com>
2026-05-22 13:18:33 +0530

    adding file

--- practics.java
+++ practics.java
@@ -1,8 +1,21 @@
 // Q1. what will be thee result of the following expression 
 // float a = 7/4 * 9/2
+import java.util.Scanner;
 public class practics{
     public static void main(String[] args ){
-        float a = 7/4*9/2;
-        System.out.println(a);
+//         float a = 7/4.0f *9/2.0f;
+//         System.out.println(a);
+//  //Q2. what a java program to encrypt a grade by adding 8 to it . Decrypt it to show the cprrect grade.
+//  //encrypte
+//         char grade = 'B';
+//         grade = (char)( grade + 8);
+//         System.out.println(grade);
+// //decrypte
+//          grade = (char)( grade - 8);
+//          System.out.println(grade);
+//Q3. use comparision operators to find out whether a given number is greater than the user enterd number or not.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);
     }
 }
---
+++ strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+    // String name = "raj";
+    // System.out.print(name);
+    // System.out.println(" my self is raj mishra");
+    // int a = 6;
+    // float b = 5.645f;
+    // System.out.printf("the value of a %d and value of b is %f", a,b);
+    Scanner sc = new Scanner(System.in);
+    // String st = sc.next();
+    String st = sc.nextLine();
+    System.out.println(st);
+    }
+
+}
