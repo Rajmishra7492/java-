2776f934d2af23246e10208421f79bcffd583ec3
day planner <rajmishra273165mkh@gmail.com>
2026-06-12 00:43:29 +0530

    updating

--- TryCatch.java
+++ TryCatch.java
@@ -1,7 +1,21 @@
 
 public class TryCatch {
     public static void main(String[] args) {
-        
+              int a = 6000;
+        int b = 0;
+//        without try
+        int c = a/b;
+        System.out.println("the result is "+c);
+//        with try
+//        try {
+//            int c = a/b;
+//            System.out.println("the result is "+c);
+//        }
+//        catch (Exception e){
+//            System.out.println("we failed to divide. reason:");
+//            System.out.println(e);
+//        }
+        System.out.println("end of the program ");
     }
     
 }
