c12b49cd8c5cba5a77e8e8020bb023048fc158da
day planner <rajmishra273165mkh@gmail.com>
2026-06-15 17:07:50 +0530

    Addng the file

---
+++ Hashing41.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class Hashing41 {
+    public static void main(String[] args){
+        // technique to convert key value too indices
+        // hashing a technique for searching the element faster and inserting an element
+        //Hashset: new empty set
+
+
+        HashSet<Integer> myHashSet = new HashSet<>(6 , 0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(3);
+        myHashSet.add(11);
+        myHashSet.add(11);
+        System.out.println(myHashSet);
+        //jitni badi list utna jyda time lagega but by the using of hashSet we can do this faster
+
+
+    }
+}
