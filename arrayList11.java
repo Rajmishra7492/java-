430348772046d47d25f6a4ee07b7edf5e9d7f543
day planner <rajmishra273165mkh@gmail.com>
2026-06-15 14:03:49 +0530

    updating

--- arrayList11.java
+++ arrayList11.java
@@ -1,8 +1,10 @@
 
 import java.util.*;
 
-public class arrayList11 {
-   public static void main(String[] args) {
+public class arrayList {
+    public static void main(String[] args) {
+        // ARRAY LIST: An ArrayList in Java is a resizable, dynamic array that implements the List interface.
+        //An ArrayList is like a resizable array
         ArrayList<Integer> l1 = new ArrayList<>();
         ArrayList<Integer> l2 = new ArrayList<>(5);
         l2.add(25);
@@ -14,22 +16,25 @@ public class arrayList11 {
         l1.add(4);
         l1.add(6);
         l1.add(5);
-        l1.add(0, 5);
-        l1.add(0, 5);
+        l1.add(0,5);
+        l1.add(0,5);
         l1.addAll(l2);
         /* l1.clear(); */
-        l1.set(1, 566);
-        l1.iterator(); // return an iterator over the elements in the list in proper sequence.
-        l1.isEmpty();// return ture if this list contains no elements.
+//        l1.set(1,566 );
+//        l1.iterator(); // return an iterator over the elements in the list in proper sequence.
+//        l1.isEmpty();// return ture if this list contains no elements.
         System.out.println(l1.lastIndexOf(5));
-        System.out.println(l1.contains(13));
-        System.out.println(l1.contains(16));
+         System.out.println(l1.contains(13));
+         System.out.println(l1.contains(16));
         System.out.println(l1.indexOf(5));
-        for (int i = 0; i < l1.size(); i++) {
+        for (int i = 0; i<l1.size(); i++) {
             System.out.println(l1.get(i));
-            System.out.print("  ");
+            System.out.print(",  ");
 
         }
     }
 
+
+
 }
+
