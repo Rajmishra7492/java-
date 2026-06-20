f979bb8ffc6afbdfc392f057db6845cf34e4614b
day planner <rajmishra273165mkh@gmail.com>
2026-06-20 23:56:03 +0530

    adding

---
+++ FileHandling.java
@@ -0,0 +1,46 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class FileHandling {
+    static void main(String[] args) {
+        
+        // File myFile = new File("raj.txt");
+        // try {
+        //     myFile.createNewFile();
+
+        // }catch (IOException e){
+        //     System.out.println("unable to create this file");
+        //     e.printStackTrace();
+        // }
+
+        // code to write a file
+
+        // try{
+        //     FileWriter fileWriter1 = new FileWriter("raj.txt");
+        //     fileWriter1.write("this is the first file which was written and made my java file handling");
+        //     fileWriter1.close();
+        // }catch (IOException e){
+        //     e.printStackTrace();
+        // }
+        
+        // READING A FILE
+        File myFile = new File("raj.txt");
+        try {
+            Scanner sc = new Scanner (myFile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        // }
+
+
+    }
+    }
+}
